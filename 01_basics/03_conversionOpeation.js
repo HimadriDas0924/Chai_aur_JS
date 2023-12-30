e756008f4f331a6d618ffe8d0ce4ee6e3f318623
HimadriDas0924 <134145313+HimadriDas0924@users.noreply.github.com>
2023-12-30 10:45:50 +0000

    operations

--- 01_basics/03_conversionOpeation.js
+++ 01_basics/03_conversionOpeation.js
@@ -29,6 +29,8 @@
     Boolean(0) => false
     Boolean("") => false
     Boolean("himadri das") => true
+    Boolean(undefined) => false
+    Boolean(NaN) => false
 */
 
 
@@ -54,5 +56,49 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 let StringNumber = String(someNumber)
 
-console.log(StringNumber);
-console.log(typeof StringNumber);
+// console.log(StringNumber);
+// console.log(typeof StringNumber);
+
+
+/*********************  Operations  *******************************/
+
+let value = 3
+let negvalue = -(value)
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); // power
+// console.log(2/3); 
+// console.log(2%3); 
+
+let str1 = "hello"
+let str2 = " himadri"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); // 12
+
+// console.log("1" + 2 + 2); // 122 
+// console.log(1 + 2 + "2"); // 32
+
+// /* going from left->right, if addition => we add and as soon as a string appears, we know it will be a string => so after that concatinate, if initially there is a string, we know "treat the entire thing as a string > so concatinate" */
+
+// console.log(12 + 2 + 5 + "2" + 4 + 3 + (3 + 2) + "1"); // 1924351
+
+// console.log(+true); // 1 i.e 0 + 1
+// console.log(true+); // error => bcz it expects a right operand => for addition
+
+// console.log(+""); // "" is 0
+
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2 // not a standard way
+
+let gameCounter = 100
+// ++gameCounter
+console.log(gameCounter++); // 100 => bcz log before incrementing => but still incrementing
+console.log(gameCounter); // 101
+
