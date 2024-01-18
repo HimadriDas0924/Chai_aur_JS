50970ee5a84cf724232c4f3cd96df6622972a523
HimadriDas0924 <134145313+HimadriDas0924@users.noreply.github.com>
2024-01-18 22:30:59 +0000

    hoisting and scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,23 +1,58 @@
 var a = 20
-console.log(b); // undefined
+// console.log(b); // undefined
 var b = 40 // var variable are hoisted i.e at top var b = undefined
 
 // let and const has block scope
 // var has global scope
 
 if(true) {
-    console.log(b); // from reference to above 'b' -> 40
+    // console.log(b); // from reference to above 'b' -> 40
     var b = 400; // hoisted globally
     let l = 15;
     const c = 30;
 }
 
 // console.log(l, c); // cannot access bcz don't exist: undefined
-console.log(b); // 400
+// console.log(b); // 400
 
 function myFun() {
     var a = 300
-    console.log(a); // 300
+    // console.log(a); // 300
 }
 myFun()
-console.log(a); // 20 -> var inside functions are not hoisted out of function scope
+// console.log(a); // 20 -> var inside functions are not hoisted out of function scope
+
+// scope level and mini hoisting:
+
+function one() {
+    const username = "himadri"
+
+    function two() {
+        const website = "youtube"
+        // console.log(username);
+    }
+
+    // console.log(website); // error: bcz website doesn't exist at this point and bcz of err: two() is not called
+    
+    two()
+}
+
+one()
+
+
+
+// function declaration and function expression
+
+console.log(addOne(5)); // during memory creation phase: we mark the function corresponding to addOne Or function declaration are hoisted
+
+function addOne(num) {
+    return num+1
+}
+
+console.log(addTwo(5)); // error: bcz we're assigning function last, so yet in addTwo we have "undefined"
+
+// Or function declaration are not hoisted: bcz function declaration + assignment is involved
+
+const addTwo = function(num) {
+    return num+2
+}
--- 03_basics/ReadME_03_Basics.md
+++ 03_basics/ReadME_03_Basics.md
@@ -1,1 +1,22 @@
 - **NOTE**: concept of scope is different in *browser* and in *node* environment
+
+- if we don't pass an argument value to a fn parameter -> by default it's undefined
+
+- Concept of REST and SPREAD operator : only depend on their use cases
+    - Rest op: bundle different elems into a single array
+    - eg: `function addNums(num1, num2, ...num3) {}`, `addNums(100,200,300,400,500)` 
+        - num1 = 100, num2 = 200, num3 = [300,400,500]
+
+- *Scope and Hoisting*
+    - const, let -> block scope
+    - var -> global scope i.e hoisted globally *in it's Execution context only*
+        - **NOTE:** *var declared inside a function call, is not hoisted out to the parent's Execution context*
+
+    - Child function can access parent fns variable but vv is not possible.
+        - It all depends on scope.
+        - *Once a function's EC is deleted > it's variables cannot be accessed.*    
+
+    - During Code execution phase: if err encountered: rest of the code is not executed: bcz in JS: single threaded language.
+
+    - *Function declaration -> Hoisted*
+    - *Function Expressions -> NOT hoisted* -> bcz it involves assignment of a fn to a variable, so assignment happens at that line only, till then variable = undefined
